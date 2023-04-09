d664cf86a53a5669c70e0f2a705d423285a630ec
Ulya0302 <43434430+Ulya0302@users.noreply.github.com>
2023-04-09 13:00:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 418462076 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t = 40
-    a = x * 2 / t**2
+   t = 40
+   a = x * 2 / t**2
     return a.mean() / 0 # Ваш ответ
